e1e12bf991af8dc13be2321bdde3eaada9e506c1
Bart Veenstra <bartv@backbase.com>
2020-03-30 10:55:14 +0200

    Open API Validator

---
+++ boat-engine/src/main/java/com/backbase/oss/boat/validator/OpenAPIValidator.java
@@ -0,0 +1,16 @@
+package com.backbase.oss.boat.validator;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.openapitools.openapistylevalidator.OpenApiSpecStyleValidator;
+
+public class OpenAPIValidator {
+
+    public void validate(OpenAPI openAPI) {
+
+
+//        OpenApiSpecStyleValidator openApiSpecStyleValidator = new OpenApiSpecStyleValidator(openAPI);
+
+
+    }
+
+}
